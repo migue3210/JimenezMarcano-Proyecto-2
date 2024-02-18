4a7dd524e360015c5db389e1d18597e50d8a60af
CarlosMarcaa <c.marcano@correo.unimet.edu.ve>
2024-02-18 19:57:58 -0400

    updates admin class :a:

--- OperativosJimenezMarcano2/src/Objects/admin.java
+++ OperativosJimenezMarcano2/src/Objects/admin.java
@@ -9,5 +9,19 @@ package Objects;
  * @author cdmar
  */
 public class Admin {
+    private Studio studio1;
+    private Studio studio2;
+    private int reviewCycle;
+
+    public Admin(Studio studio1,Studio studio2) {
+        this.studio1 = studio1;
+        this.studio2 = studio2;
+    }
     
+    public void updateInitialQueues(StudioCharacter character1, StudioCharacter character2){
+        //Depending on the overallQuality of the characters, they will be added to their corresponding queue
+        if (character1.getOverallQuality() == 1) {
+            studio1.getPrior3Queue().enqueue(character1);
+        }
+    }
 }
